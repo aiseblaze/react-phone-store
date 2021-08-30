38f5ce23b26a4a0b3319a753072ed56a1a185463
Blaise Etongwe <blaiseetongwe@gmail.com>
2021-08-30 14:25:32 +0100

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Hello from about</h2>
+            </div>
+        )
+    }
+}
